9722ed17012e8901a066b1e417783226e31394bf
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-04-04 15:49:59 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red border-2 border-red-800">Hello</h1>
+    </div>
+  );
+};
+
+export default App;
