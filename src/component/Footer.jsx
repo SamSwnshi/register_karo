5e9e4eb4a23a35f35d17014ab9e2b819b37239c9
Sameer <114623580+SamSwnshi@users.noreply.github.com>
2025-04-05 19:00:23 +0530

    footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,11 +1,103 @@
-import React from 'react'
+import React from "react";
 
 const Footer = () => {
   return (
-    <div className="px-28 h-screen">
-      
+    <div className="px-32 h-screen flex flex-col bg-[#0D1321] text-white">
+      <div className="flex justify-between ">
+        <div>
+          <div>
+            <p>START A BUSINESS</p>
+          </div>
+          <div>
+            <p>Private Limited Company</p>
+            <p>Limited Liability Partnership</p>
+            <p>One Person Company</p>
+            <p>Section 8 Company Registration</p>
+            <p>Producer Company Registration</p>
+            <p>MSME Registration</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>GOVERNMENT REGISTRATION</p>
+          </div>
+          <div>
+            <p>FSSAI Registration</p>
+            <p>IEC Registration</p>
+            <p>Trademark Registration</p>
+            <p>DSC Registration</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>COMPLIANCE & TAX</p>
+          </div>
+          <div>
+            <p>Compliance Pvt Ltd</p>
+            <p>Compliance LLP</p>
+            <p>GST Registration</p>
+          </div>
+        </div>
+        <div>
+          <div>
+            <p>START A BUSINESS</p>
+          </div>
+          <div>
+            <p>BIS Certification</p>
+            <p>BIS FMCS Certification</p>
+            <p>BIS CRS Certification</p>
+            <p>ISI Mark Certification</p>
+            <p>CDSCO Registration</p>
+            <p>Medical Device Registration</p>
+          </div>
+        </div>
+      </div>
+      <button>See all the Services</button>
+      <hr />
+      <div>
+        <div className="icons"></div>
+        <div>
+          <p>Elevate Your Business with RegisterKaro</p>
+          <p>As a leading technology-driven legal services and advisory firm, we empower SMEs and entrepreneurs on their business journey. Our expert team covers business registration, legal compliance, tax filing, IPR registration, and more. With over 200 professionals, we've served 50,000+ satisfied customers, ensuring startup compliance with our country's legal and regulatory systems.</p>
+        </div>
+        <div>
+          <p>Environmental Solutions</p>
+          <p>RegisterKaro offers a comprehensive range of services to address environmental challenges in business. Our seasoned environmental experts, with over a decade of experience, provide comprehensive solutions for environmental compliance and advisory, including battery waste management, plastic waste management, and e-waste management.</p>
+        </div>
+        <div>
+          <p>Navigating Regulatory Affairs</p>
+          <p>In India, regulatory bodies like BIS, CDSCO, RBI, SEBI, and IRDAI hold the keys to licenses and registrations for banks, financial institutions, and insurance businesses. We simplify this process by connecting you with our legal professionals. We understand your needs, handle license or registration applications, liaise with authorities, and deliver the licenses you require.</p>
+        </div>
+        <div>
+          <p>Business Registration Expertise</p>
+          <p>We are renowned for facilitating business registration, whether it's as a private limited company, one-person company, Section 8 company, LLP, public company, or Nidhi company. Our consultancy services extend from business setup from scratch to ongoing compliance.</p>
+        </div>
+        <div>
+          <p>Safeguarding Intellectual Property</p>
+          <p>Intellectual property protection is vital for modern businesses. Our team excels in IP registration services such as trademark registration, handling objections, managing assignments, copyright registration, and patent registration.</p>
+        </div>
+        <div>
+          <p>Simplifying Taxation</p>
+          <p>RegisterKaro is your all-in-one solution for tax-related needs. Our dedicated professionals assist with GST registration, professional tax registration, GST return filing, TDS return filing, income tax return filing, and secretarial audits. Your tax matters are in capable hands with RegisterKaro.</p>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-col items-center">
+        <p>
+          By continuing this page, you agree to our Terms & Conditions, Privacy
+          Policy and Refund Policy.
+        </p>
+        <p>© 2025 - Safe Ledger Private Limited. All rights reserved.</p>
+        <p>
+          Please note that we are a facilitating platform enabling access to
+          reliable professionals. We are not a law firm and do not provide legal
+          services ourselves. The information on this website is for the purpose
+          of knowledge only and should not be relied upon as legal advice or
+          opinion.
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
